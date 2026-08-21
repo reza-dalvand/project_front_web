1d9ef1fbcb6dd65eb1488ddbd466e16300b82085
reza <deploy@srv8969380876.bitcommand.net>
2026-08-21 07:13:56 +0000

    push in server

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: 'beau-club',
+    script: 'npm',
+    args: 'start',
+    env: {
+      NODE_ENV: 'production',
+    },
+    instances: 'max', // استفاده از تمام CPU cores
+    exec_mode: 'cluster',
+    max_memory_restart: '1G',
+  }]
+}
