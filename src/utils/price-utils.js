5edac4944b98b30e19eab60d7404e778e4bb18cd
Reza D <rdalvand@yahoo.com>
2026-09-15 19:36:20 +0330

    change app fee 2

--- src/utils/price-utils.js
+++ src/utils/price-utils.js
@@ -38,7 +38,7 @@ export const calculateAppFee = (basePrice) => {
   let fee = 0;
 
   if (basePrice < 250000) {
-    fee = 10000;
+    fee = 9000;
   } else if (basePrice <= 500000) {
     // ✅ ۳ درصد
     fee = Math.round(basePrice * 0.03);
@@ -60,7 +60,7 @@ export const APP_FEE_TIERS = [
     fee: 9000,
     type: 'fixed',
     label: 'ثابت',
-    description: '۷ هزار تومان',
+    description: '۹ هزار تومان',
   },
   {
     min: 250000,
